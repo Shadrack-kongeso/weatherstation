5cadb0d71813c39cd1211cc0e1189a8ba059867f
Bernard Nandwa <bernard@nandwa.com>
2021-09-16 16:28:37 +0300

    Define weather object

--- weather_station.js
+++ weather_station.js
@@ -1,8 +1,64 @@
+/**
+ * Define the weather machine object.
+ * 
+ * Show the current weather in different locations. [locations, time - current(live/realtime), weather]
+ */
+const WeatherMachine = {
+    location: {
+        items: [
+            {
+                name: 'Nairobi',
+                longitude: '5',
+                latitude: '0'
+            },
+            {
+                name: 'Kisumu',
+                longitude: '15',
+                latitude: '0'
+            }
+        ],
+        currentLocation: 'Nairobi',
+        // Do
+        switchLocation: () => {
+            // Change the current location attribute
+
+            // Get the select element
+
+            // Listen for changes. Add event listener
+
+            // Get the value selected by the user
+
+            // Set the value of the currentLocation
+        }
+    },
+    time: {
+        months: ['January']
+    },
+    weather: {
+        currentWeather: {},
+        samples: [],
+
+        // Add function for fetching weather
+        fetchCurrentWeather: (location, time) => {
+            // Check the current time
+
+            // If the time is less than today; show historical weather
+
+            // If the time is greater than now; show forecast
+
+            // 
+        }
+    }
+}
+
+
 const button = document.getElementById("weather_machine");
+
 
 // event handler
 const hoverOverButton = () => {
-    console.log('Hover')
+    // maizePlant.harvest();
+    console.log('Height: ', maizePlant.height)
 
     // 1. Get the div where we are going to write text
     const weather_station = document.getElementById("content");
@@ -13,4 +69,4 @@ const hoverOverButton = () => {
 }
 
 // Add an event listener
-button.addEventListener('mouseover', hoverOverButton)
+button.addEventListener('mouseover', hoverOverButton);
